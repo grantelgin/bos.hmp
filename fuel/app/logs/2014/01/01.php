79b4157cd7d6e42574865965f77e25afab07743c
grantelgin <grantelgin@gmail.com>
2014-01-01 10:53:01 -0500

    asset issue

---
+++ fuel/app/logs/2014/01/01.php
@@ -0,0 +1,10 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-01 10:48:41 --> Parsing Error - syntax error, unexpected '(', expecting identifier (T_STRING) or variable (T_VARIABLE) or '{' or '$' in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:49:03 --> Fatal Error - Call to undefined function Asset() in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:50:02 --> Warning - include_once(../assets/js/analyticstracking.php): failed to open stream: No such file or directory in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:51:06 --> Warning - include_once(): http:// wrapper is disabled in the server configuration by allow_url_include=0 in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:51:26 --> Warning - include_once(//bos.hotmusicproducer.com/assets/js/analyticstracking.php): failed to open stream: No such file or directory in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:51:34 --> Warning - include_once(bos.hotmusicproducer.com/assets/js/analyticstracking.php): failed to open stream: No such file or directory in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:51:51 --> Warning - include_once(/assets/js/analyticstracking.php): failed to open stream: No such file or directory in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
+ERROR - 2014-01-01 10:51:58 --> Warning - include_once(public/assets/js/analyticstracking.php): failed to open stream: No such file or directory in /Users/grantelgin/Sites/bos.hotmusicproducer/fuel/app/views/welcome/index.php on line 15
